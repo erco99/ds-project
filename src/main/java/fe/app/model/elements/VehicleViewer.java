a82df351b448c72acd7dab261db720c46b537e84
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-09-25 11:42:54 +0200

    Create viewer for position updates and panel for visual drawing

--- src/main/java/fe/app/model/elements/VehicleViewer.java
+++ src/main/java/fe/app/model/elements/VehicleViewer.java
@@ -1,2 +1,33 @@
-package fe.app.model.elements;public class VehicleViewer {
+package fe.app.model.elements;
+
+import fe.app.view.MapPanel;
+
+public class VehicleViewer extends Thread {
+
+    private static final int FRAMES_PER_SEC = 30;
+    private boolean stop;
+    private MapPanel mapPanel;
+    private MapContext mapContext;
+
+    public VehicleViewer(MapPanel mapPanel, MapContext mapContext) {
+        this.stop = false;
+        this.mapPanel = mapPanel;
+        this.mapContext = mapContext;
+    }
+
+    public void run() {
+        while (!stop) {
+            long t0 = System.currentTimeMillis();
+            mapPanel.updateVehiclesPosition(mapContext.getVehiclesPosition());
+            long t1 = System.currentTimeMillis();
+            //log("update pos");
+            long dt = (1000 / FRAMES_PER_SEC) - (t1-t0);
+            if (dt > 0){
+                try {
+                    Thread.sleep(dt);
+                } catch (Exception ex){
+                }
+            }
+        }
+    }
 }
--- src/main/java/fe/app/view/MapPanel.java
+++ src/main/java/fe/app/view/MapPanel.java
@@ -1,2 +1,58 @@
-package fe.app.view;public class MapPanel {
+package fe.app.view;
+
+import fe.app.util.Pair;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+
+public class MapPanel extends JPanel {
+
+    private ArrayList<Pair<Double, Double>> positions;
+    private static final int WIDTH_DIMENSION = 800;
+    private static final int HEIGHT_DIMENSION = 800;
+    private long dx;
+    private long dy;
+    public MapPanel() {
+        this.setPreferredSize(new Dimension(WIDTH_DIMENSION,HEIGHT_DIMENSION));
+        dx = WIDTH_DIMENSION/2 - 20;
+        dy = HEIGHT_DIMENSION/2 - 20;
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setRenderingHint(RenderingHints.KEY_RENDERING,
+                RenderingHints.VALUE_RENDER_QUALITY);
+        g2.clearRect(0,0,this.getWidth(),this.getHeight());
+        synchronized (this){
+            if (positions!=null){
+                positions.forEach(p -> {
+                    int x0 = (int)(dx+p.getX()*dx);
+                    int y0 = (int)(dy-p.getY()*dy);
+
+
+                    final AffineTransform saved = g2.getTransform();
+                    final AffineTransform rotate = AffineTransform.getRotateInstance(
+                            Math.toRadians(360), x0, y0);
+                    g2.transform(rotate);
+
+                    Rectangle rect = new Rectangle(x0,y0,20,10);
+                    g2.draw(rect);
+                    g2.setTransform(saved);
+                });
+            }
+        }
+    }
+
+    public void updateVehiclesPosition(ArrayList<Pair<Double, Double>> positions){
+        synchronized(this){
+            this.positions = positions;
+        }
+        System.out.println(this.positions);
+        repaint();
+    }
 }
