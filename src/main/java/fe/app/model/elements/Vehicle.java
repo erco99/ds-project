06f4e939a28c8cf61c5b86cc15804071b5128660
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-09-25 11:41:44 +0200

    Create element for vehicle behavior and design

--- src/main/java/fe/app/model/elements/Vehicle.java
+++ src/main/java/fe/app/model/elements/Vehicle.java
@@ -1,9 +1,44 @@
-package model.elements;
+package fe.app.model.elements;
 
-public class Vehicle {
+import fe.app.util.Pair;
 
-    private int x;
-    private int y;
+public class Vehicle extends Thread {
+
+    private Pair<Double,Double> position;
+    private boolean stop;
+
+    private MapContext mapContext;
+
+    public Vehicle(MapContext mapContext) {
+        this.mapContext = mapContext;
+        this.position = new Pair<>(0.0,0.0);
+        this.stop = false;
+    }
+
+    public void run() {
+        //log("INIT: vel "+vel+"speed "+speed);
+        try {
+            while (!this.stop){
+                updatePosition();
+                // log("pos updated: "+pos);
+                Thread.sleep(5);
+            }
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    public void terminate() {
+        this.stop = true;
+    }
+
+    private void updatePosition() {
+        this.position = new Pair<>(this.position.getX() , this.position.getY() + 0.00036);
+    }
+
+    public Pair<Double, Double> getPosition() {
+        return this.position;
+    }
 
 
 }
