17edfa20e268b72404b63055cd4bc3c1f2829ceb
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-09-25 11:39:21 +0200

    Create view and controller

--- src/main/java/fe/app/controller/Controller.java
+++ src/main/java/fe/app/controller/Controller.java
@@ -1,8 +1,25 @@
-package controller;
+package fe.app.controller;
+
+import fe.app.model.elements.VehicleViewer;
+import fe.app.model.elements.MapContext;
+import fe.app.view.View;
 
 public class Controller {
 
-    public void start() {
+    View view;
 
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public void start() {
+        MapContext mapContext = new MapContext();
+
+        view.start();
+        VehicleViewer vehicleViewer = new VehicleViewer(view.getMapPanel(), mapContext);
+        vehicleViewer.start();
+
+
+        mapContext.addVehicle();
     }
 }
--- src/main/java/fe/app/view/View.java
+++ src/main/java/fe/app/view/View.java
@@ -1,4 +1,28 @@
-package view;
+package fe.app.view;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class View {
+
+    MapPanel mapPanel;
+    static JFrame frame;
+
+    public void start() {
+        frame = new JFrame("App");
+        frame.setLayout(new BorderLayout());
+
+        mapPanel = new MapPanel();
+        frame.add(mapPanel);
+
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+
+    public MapPanel getMapPanel()    {
+        return mapPanel;
+    }
+
 }
