eafbbcbdaa3cf5c0e8c8eb0c8b3a56d4648f58ff
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-09-29 16:56:19 +0200

    Add left and right directions of travel

--- src/main/java/fe/app/model/elements/Street.java
+++ src/main/java/fe/app/model/elements/Street.java
@@ -5,25 +5,42 @@ import fe.app.util.Pair;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
+import java.util.Stack;
 
 public class Street {
 
-    public static final int ROADWAY_SIZE = 40;
+    public static final int ROADWAY_SIZE = 42;
+    private static final int STREETSIDE_DISTANCE = 14;
     private String id;
     private Line firstSide;
     private Line secondSide;
+    private DirectionLine rightWay;
+    private DirectionLine leftWay;
 
     public Street(String id, int width, int height, Pair<Integer, Integer> startingPoint) {
         this.id =  id;
 
         if(startingPoint.getX() == 0) {
             Integer y = startingPoint.getY();
+
             firstSide = new Line(startingPoint.getX(), y, width, y);
+            leftWay = new DirectionLine(new Pair<>(width, y + STREETSIDE_DISTANCE),
+                    new Pair<>(startingPoint.getX(), y + STREETSIDE_DISTANCE));
+
             secondSide = new Line(startingPoint.getX(), y + ROADWAY_SIZE, width, y + ROADWAY_SIZE);
+            rightWay = new DirectionLine(new Pair<>(width, y + ROADWAY_SIZE - STREETSIDE_DISTANCE),
+                    new Pair<>(startingPoint.getX(), y + ROADWAY_SIZE - STREETSIDE_DISTANCE));
         } else {
             Integer x = startingPoint.getX();
+
             firstSide = new Line(x, startingPoint.getY(), x, height);
+            leftWay = new DirectionLine(new Pair<>(x + STREETSIDE_DISTANCE, startingPoint.getY()),
+                    new Pair<>(x + STREETSIDE_DISTANCE, height));
+
             secondSide = new Line(x + ROADWAY_SIZE, startingPoint.getY(), x + ROADWAY_SIZE, height);
+            rightWay = new DirectionLine(new Pair<>(x + ROADWAY_SIZE - STREETSIDE_DISTANCE, startingPoint.getY()),
+                    new Pair<>(x + ROADWAY_SIZE - STREETSIDE_DISTANCE, height));
+
         }
     }
 
@@ -33,6 +50,14 @@ public class Street {
 
     public Line getSecondSide() {
         return secondSide;
+    }
+
+    public DirectionLine getRightWay() {
+        return rightWay;
+    }
+
+    public DirectionLine getLeftWay() {
+        return leftWay;
     }
 
     public String getId() {
