3313e7ba5a565ea329a5e362ed0de4157d5927c1
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-09-29 13:40:14 +0200

    Add creation of horizontal and vertical streets and add getters

--- src/main/java/fe/app/model/elements/Street.java
+++ src/main/java/fe/app/model/elements/Street.java
@@ -1,15 +1,43 @@
 package fe.app.model.elements;
 
+import fe.app.util.Pair;
+
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Street {
 
-    public ArrayList<Line> firstSide;
-    public ArrayList<Line> secondSide;
+    public static final int ROADWAY_SIZE = 40;
+    private String id;
+    private Line firstSide;
+    private Line secondSide;
 
-    public Street() {
+    public Street(String id, int width, int height, Pair<Integer, Integer> startingPoint) {
+        this.id =  id;
 
+        if(startingPoint.getX() == 0) {
+            Integer y = startingPoint.getY();
+            firstSide = new Line(startingPoint.getX(), y, width, y);
+            secondSide = new Line(startingPoint.getX(), y + ROADWAY_SIZE, width, y + ROADWAY_SIZE);
+        } else {
+            Integer x = startingPoint.getX();
+            firstSide = new Line(x, startingPoint.getY(), x, height);
+            secondSide = new Line(x + ROADWAY_SIZE, startingPoint.getY(), x + ROADWAY_SIZE, height);
+        }
     }
+
+    public Line getFirstSide() {
+        return firstSide;
+    }
+
+    public Line getSecondSide() {
+        return secondSide;
+    }
+
+    public String getId() {
+        return id;
+    }
+
 
 }
