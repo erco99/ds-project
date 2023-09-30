9067be0690f3569d1771875c712f193bea5338cb
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-09-30 15:15:59 +0200

    Add the identifications of single intersections between directions

--- src/main/java/fe/app/model/elements/StreetMap.java
+++ src/main/java/fe/app/model/elements/StreetMap.java
@@ -4,7 +4,6 @@ import fe.app.util.LinesUtil;
 import fe.app.util.Pair;
 
 import java.awt.*;
-import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -15,11 +14,14 @@ public class StreetMap {
     private final Random random = new Random();
     private final ArrayList<Street> horizontalStreets;
     private final ArrayList<Street> verticalStreets;
-    private final ArrayList<Polygon> intersections;
+    private final ArrayList<Polygon> streetSidesIntersections;
+
+    private final ArrayList<Intersection> intersections;
 
     public StreetMap() {
         this.horizontalStreets = new ArrayList<>();
         this.verticalStreets = new ArrayList<>();
+        this.streetSidesIntersections = new ArrayList<>();
         this.intersections = new ArrayList<>();
     }
 
@@ -41,13 +43,35 @@ public class StreetMap {
     }
 
     private void identifyIntersections() {
-        for (Street hstreet : this.horizontalStreets) {
-            for (Street vstreet: this.verticalStreets) {
+        for (Street hStreet : this.horizontalStreets) {
+            for (Street vStreet: this.verticalStreets) {
+                Line hFirstSide = hStreet.getFirstSide();
+                Line vFirstSide = vStreet.getFirstSide();
+
+                DirectionLine hLeftWay = hStreet.getLeftWay();
+                DirectionLine hRightWay = hStreet.getRightWay();
+
+                DirectionLine vLeftWay = vStreet.getLeftWay();
+                DirectionLine vRightWay = vStreet.getRightWay();
+
                 Pair<Integer,Integer> point = LinesUtil.lineLineIntersection(
-                    new Pair<>(hstreet.getFirstSide().x1, hstreet.getFirstSide().y1),
-                    new Pair<>(hstreet.getFirstSide().x2, hstreet.getFirstSide().y2),
-                    new Pair<>(vstreet.getFirstSide().x1, vstreet.getFirstSide().y1),
-                    new Pair<>(vstreet.getFirstSide().x2, vstreet.getFirstSide().y2)
+                    new Pair<>(hFirstSide.x1, hFirstSide.y1),
+                    new Pair<>(hFirstSide.x2, hFirstSide.y2),
+                    new Pair<>(vFirstSide.x1, vFirstSide.y1),
+                    new Pair<>(vFirstSide.x2, vFirstSide.y2)
+                );
+
+                intersections.add(
+                        new Intersection(getIntersectionPoint(hLeftWay,vLeftWay), new Pair<>(hStreet, vStreet))
+                );
+                intersections.add(
+                        new Intersection(getIntersectionPoint(hLeftWay,vRightWay), new Pair<>(hStreet, vStreet))
+                );
+                intersections.add(
+                        new Intersection(getIntersectionPoint(hRightWay,vLeftWay), new Pair<>(hStreet, vStreet))
+                );
+                intersections.add(
+                        new Intersection(getIntersectionPoint(hRightWay,vRightWay), new Pair<>(hStreet, vStreet))
                 );
 
                 Polygon p = new Polygon();
@@ -55,10 +79,20 @@ public class StreetMap {
                 p.addPoint(point.getX() + Street.ROADWAY_SIZE, point.getY());
                 p.addPoint(point.getX() + Street.ROADWAY_SIZE, point.getY() + Street.ROADWAY_SIZE);
                 p.addPoint(point.getX(), point.getY() + Street.ROADWAY_SIZE);
-                this.intersections.add(p);
+                this.streetSidesIntersections.add(p);
             }
         }
     }
+
+    private Pair<Integer,Integer> getIntersectionPoint(Line a, Line b) {
+        return LinesUtil.lineLineIntersection(
+            new Pair<>(a.x1, a.y1),
+            new Pair<>(a.x2, a.y2),
+            new Pair<>(b.x1, b.y1),
+            new Pair<>(b.x2, b.y2)
+        );
+    }
+
     public ArrayList<Street> getHorizontalStreets() {
         return this.horizontalStreets;
     }
@@ -67,9 +101,11 @@ public class StreetMap {
         return verticalStreets;
     }
 
-    public ArrayList<Polygon> getIntersections() {
-        return intersections;
+    public ArrayList<Polygon> getStreetSidesIntersections() {
+        return streetSidesIntersections;
     }
 
-
+    public ArrayList<Intersection> getIntersections() {
+        return intersections;
+    }
 }
