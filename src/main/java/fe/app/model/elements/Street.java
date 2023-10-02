bb4b5c9fd00ebfe6103b889eabf4a18afcc4653c
Author Name <francesco.ercolani3@studio.unibo.it>
2023-10-02 12:59:50 +0200

    Add street type

--- src/main/java/fe/app/model/elements/Street.java
+++ src/main/java/fe/app/model/elements/Street.java
@@ -1,22 +1,25 @@
 package fe.app.model.elements;
 
 import fe.app.util.Pair;
+import fe.app.util.StreetType;
 
 public class Street {
 
     public static final int ROADWAY_SIZE = 42;
     private static final int STREET_SIDE_DISTANCE = 14;
-    private String id;
-    private Line firstSide;
-    private Line secondSide;
-    private DirectionLine rightWay;
-    private DirectionLine leftWay;
+    private final String id;
+    private final Line firstSide;
+    private final Line secondSide;
+    private final DirectionLine rightWay;
+    private final DirectionLine leftWay;
+    private final String streetType;
 
     public Street(String id, int width, int height, Pair<Integer, Integer> startingPoint) {
         this.id =  id;
 
         if(startingPoint.getX() == 0) {
             Integer y = startingPoint.getY();
+            this.streetType = String.valueOf(StreetType.HORIZONTAL);
 
             firstSide = new Line(startingPoint.getX(), y, width, y);
             leftWay = new DirectionLine(this.id, new Pair<>(width, y + STREET_SIDE_DISTANCE),
@@ -27,13 +30,14 @@ public class Street {
                     new Pair<>(startingPoint.getX(), y + ROADWAY_SIZE - STREET_SIDE_DISTANCE));
         } else {
             Integer x = startingPoint.getX();
+            this.streetType = String.valueOf(StreetType.VERTICAL);
 
             firstSide = new Line(x, startingPoint.getY(), x, height);
-            leftWay = new DirectionLine(this.id, new Pair<>(x + STREET_SIDE_DISTANCE, startingPoint.getY()),
+            rightWay = new DirectionLine(this.id, new Pair<>(x + STREET_SIDE_DISTANCE, startingPoint.getY()),
                     new Pair<>(x + STREET_SIDE_DISTANCE, height));
 
             secondSide = new Line(x + ROADWAY_SIZE, startingPoint.getY(), x + ROADWAY_SIZE, height);
-            rightWay = new DirectionLine(this.id, new Pair<>(x + ROADWAY_SIZE - STREET_SIDE_DISTANCE, startingPoint.getY()),
+            leftWay = new DirectionLine(this.id, new Pair<>(x + ROADWAY_SIZE - STREET_SIDE_DISTANCE, startingPoint.getY()),
                     new Pair<>(x + ROADWAY_SIZE - STREET_SIDE_DISTANCE, height));
         }
     }
@@ -54,6 +58,9 @@ public class Street {
         return leftWay;
     }
 
+    public String getStreetType() {
+        return streetType;
+    }
     public String getId() {
         return id;
     }
