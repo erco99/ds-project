c06f2cd10319063c5a5190f6c13cad046ed1c88c
Francesco Ercolani <francesco.ercolani3@studio.unibo.it>
2023-10-20 17:30:26 +0200

    Add methods to gsonutils

--- src/main/java/fe/app/util/GsonUtils.java
+++ src/main/java/fe/app/util/GsonUtils.java
@@ -1,7 +1,6 @@
 package fe.app.util;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.*;
 import fe.app.model.tfmanagement.presentation.Request;
 import fe.app.model.tfmanagement.presentation.RequestDeserializer;
 
@@ -13,5 +12,23 @@ public class GsonUtils {
                 .serializeNulls()
                 .registerTypeAdapter(Request.class, new RequestDeserializer())
                 .create();
+    }
+
+    public static String getPropertyAsString(JsonObject object, String name) {
+        if (object.has(name)) {
+            JsonElement value = object.get(name);
+            if (value.isJsonNull()) return null;
+            return value.getAsString();
+        }
+        return null;
+    }
+
+    public static <T> T getPropertyAs(JsonObject object, String name, Class<T> type,  JsonDeserializationContext context) {
+        if (object.has(name)) {
+            JsonElement value = object.get(name);
+            if (value.isJsonNull()) return null;
+            return context.deserialize(value, type);
+        }
+        return null;
     }
 }
